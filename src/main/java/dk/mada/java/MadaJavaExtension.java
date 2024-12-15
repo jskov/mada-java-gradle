4afebe039c1c462767a4ce079742a91ba20c0177
Jesper Skov <jskov@mada.dk>
2024-12-15 18:21:33 +0100

    Add DSL for configuration of POMs and publishing to directory (#14)

---
+++ src/main/java/dk/mada/java/MadaJavaExtension.java
@@ -0,0 +1,44 @@
+package dk.mada.java;
+
+import java.io.File;
+
+import org.gradle.api.Action;
+import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.tasks.Nested;
+
+import dk.mada.java.dsl.Pom;
+
+/**
+ * Extension for mada-java plugin.
+ */
+public abstract class MadaJavaExtension {
+    /** Creates a new instance. */
+    public MadaJavaExtension() { // NOSONAR - must be public for Gradle
+        // empty
+    }
+
+    /** {@return an optional directory to publish to} */
+    public abstract DirectoryProperty getPublishTo();
+
+    /**
+     * Allow DSL to set publish-to property.
+     *
+     * @param dir the directory to publish to
+     */
+    public void publishTo(File dir) {
+        getPublishTo().set(dir);
+    }
+
+    /** {@return the Pom configuration} */
+    @Nested
+    public abstract Pom getPom();
+
+    /**
+     * Process new Pom action.
+     *
+     * @param action the action for the Pom
+     */
+    public void pom(Action<? super Pom> action) {
+        action.execute(getPom());
+    }
+}
--- src/main/java/dk/mada/java/MadaJavaPlugin.java
+++ src/main/java/dk/mada/java/MadaJavaPlugin.java
@@ -1,24 +1,104 @@
 package dk.mada.java;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.publish.PublishingExtension;
+import org.gradle.api.publish.maven.MavenPom;
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
+import org.gradle.api.publish.maven.tasks.GenerateMavenPom;
+import org.gradle.api.publish.plugins.PublishingPlugin;
 import org.gradle.api.tasks.compile.JavaCompile;
+import org.jspecify.annotations.Nullable;
+
+import dk.mada.java.dsl.Pom;
+import dk.mada.java.dsl.PomDeveloperSpec.Developer;
+import dk.mada.java.dsl.PomLicenseSpec.License;
+import dk.mada.java.dsl.PomScm;
 
 /**
  * A plugin defining the java conventions used for dk.mada projects.
  */
 public final class MadaJavaPlugin implements Plugin<Project> {
-    /** Constructs new instance. */
-    public MadaJavaPlugin() {
-        // empty
+    /**
+     * Constructs new instance.
+     */
+    @Inject
+    public MadaJavaPlugin() { // NOSONAR - public for Gradle
     }
 
     @Override
     public void apply(Project project) {
+        Logger logger = project.getLogger();
+
+        logger.info("mada-java applying plugin");
+
+        MadaJavaExtension ext = project.getExtensions().create("madaJava", MadaJavaExtension.class);
+
+        project.afterEvaluate(p -> p.getPlugins().withType(PublishingPlugin.class, mpp -> configurePublishing(p, ext)));
+        project.afterEvaluate(p -> p.getPlugins().withType(MavenPublishPlugin.class, mpp -> configureMavenPublishing(p, ext)));
+
         project.getPlugins().withType(JavaPlugin.class, jp -> applyPlugins(project));
+    }
+
+    private void configurePublishing(Project project, MadaJavaExtension ext) {
+        PublishingExtension publishingExt = project.getExtensions().getByType(PublishingExtension.class);
+
+        if (ext.getPublishTo().isPresent()) {
+            DirectoryProperty publishTo = ext.getPublishTo();
+            MavenArtifactRepository mar = project.getRepositories().maven(m -> {
+                m.setName("dist");
+                m.setUrl(publishTo);
+            });
+            project.getLogger().info("mada-java publish to {}", publishTo.get());
+            publishingExt.getRepositories().add(mar);
+        }
+    }
+
+    private void configureMavenPublishing(Project project, MadaJavaExtension ext) {
+        Logger logger = project.getLogger();
+
+        Pom conventionPom = ext.getPom();
+        List<Developer> conventionDevelopers = conventionPom.getDevelopers();
+        List<License> conventionLicenses = ext.getPom().getLicenses();
+        @Nullable PomScm conventionScm = ext.getPom().getScm();
+
+        project.getTasks().withType(GenerateMavenPom.class).configureEach(pt -> {
+            logger.info("mada-java configuring task {}", pt.getName());
+            MavenPom pom = pt.getPom();
+
+            pom.getName().set(conventionPom.getName());
+            pom.getUrl().set(conventionPom.getUrl());
+            pom.getDescription().set(conventionPom.getDescription());
+            pom.setPackaging(conventionPom.getPackaging().getOrNull());
+
+            pom.developers(developer -> conventionDevelopers.forEach(conventionDeveloper -> developer.developer(dev -> {
+                dev.getId().set(conventionDeveloper.getId());
+                dev.getName().set(conventionDeveloper.getName());
+                dev.getEmail().set(conventionDeveloper.getEmail());
+            })));
+
+            pom.licenses(license -> conventionLicenses.forEach(conventionLicense -> license.license(lic -> {
+                lic.getName().set(conventionLicense.getName());
+                lic.getUrl().set(conventionLicense.getUrl());
+            })));
+
+            if (conventionScm != null) {
+                pom.scm(scm -> {
+                    scm.getConnection().set(conventionScm.getConnection());
+                    scm.getDeveloperConnection().set(conventionScm.getDeveloperConnection());
+                    scm.getUrl().set(conventionScm.getUrl());
+                });
+            }
+        });
     }
 
     private void applyPlugins(Project project) {
